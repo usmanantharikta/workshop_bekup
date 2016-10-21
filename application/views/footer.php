5859569fec771de51bdaa07ec049fe8dbb65d1e3
usmanantharikta <antharikta7@gmail.com>
2016-10-21 17:32:17 +0700

    the first upload

---
+++ application/views/footer.php
@@ -0,0 +1,10 @@
+<script src="<?php echo base_url('assets/js/bootstrap.js');?>"></script>
+<!-- material design -->
+<script src="<?php echo base_url('assets/js/material.js');?>"></script>
+<script src="<?php echo base_url('assets/js/ripples.js');?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap-datepicker.js');?>"></script>
+<script>
+  $.material.init();
+</script>
+</body>
+</html>
