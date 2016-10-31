e265f8c7a6037dfca140d1badfca35e64c3d9115
usmanantharikta <antharikta7@gmail.com>
2016-10-31 20:45:06 +0700

    full version 1

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'antharikta',
-	'database' => 'workshop_bekup',
+	'hostname' => 'http://rezamuzay.id/',
+	'username' => 'bekup',
+	'password' => 'y2u7udu8e',
+	'database' => 'zadmin_bekup',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
